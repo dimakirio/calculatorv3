201840a8cb1e0f162786ab1ee8771d77b9fd3e4f
dimakirio <kirionenkodima@gmail.com>
2025-05-07 17:58:13 +0500

    Update main.go

--- cmd/orchestrator/main.go
+++ cmd/orchestrator/main.go
@@ -1,1 +1,15 @@
+package main
 
+import (
+	"log"
+
+	"github.com/dimakirio/calc_go/internal/application"
+)
+
+func main() {
+	app := application.NewOrchestrator()
+	log.Println("Starting Orchestrator on port", app.Config.Addr)
+	if err := app.RunServer(); err != nil {
+		log.Fatal(err)
+	}
+}
