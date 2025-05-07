2f155ba6f0d1a14e09b0ea4efa1cd6ea1c2a9675
dimakirio <kirionenkodima@gmail.com>
2025-05-07 17:57:38 +0500

    Update main.go

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,1 +1,13 @@
+package main
 
+import (
+	"log"
+
+	"github.com/dimakirio/calc_go/internal/application"
+)
+
+func main() {
+	agent := application.NewAgent()
+	log.Println("Starting Agent...")
+	agent.Run()
+}
