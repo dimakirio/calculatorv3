193d61949afa8e153981a601341f84db3abc9c9d
dimakirio <kirionenkodima@gmail.com>
2025-05-11 21:38:29 +0500

    Update logger.go

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,8 +1,27 @@
-package calculation
+package logger
 
-import "errors"
-
-var (
-	ErrDivisionByZero  = errors.New("division by zero")
-	ErrInvalidOperator = errors.New("invalid operator")
+import (
+	"log"
+	"os"
 )
+
+type Logger struct {
+	*log.Logger
+}
+
+func NewLogger(level string) *Logger {
+	return &Logger{log.New(os.Stdout, "", log.LstdFlags)}
+}
+
+func (l *Logger) Info(msg string) {
+	l.Println("INFO: " + msg)
+}
+
+func (l *Logger) Error(msg string) {
+	l.Println("ERROR: " + msg)
+}
+
+func (l *Logger) Fatal(msg string) {
+	l.Println("FATAL: " + msg)
+	os.Exit(1)
+}
