56bdd21d51eeabbe2834dc7ae5cd71fb9d0d97c8
dimakirio <kirionenkodima@gmail.com>
2025-05-11 21:31:27 +0500

    Update jwt.go

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -1,148 +1,54 @@
-package application
+package auth
 
 import (
-	"fmt"
-	"strconv"
-	"strings"
-	"unicode"
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
 )
 
-type ASTNode struct {
-	IsLeaf        bool
-	Value         float64
-	Operator      string
-	Left, Right   *ASTNode
-	TaskScheduled bool
+type JWTService struct {
+	secretKey []byte
 }
 
-func ParseAST(expression string) (*ASTNode, error) {
-	expr := strings.ReplaceAll(expression, " ", "")
-	if expr == "" {
-		return nil, fmt.Errorf("empty expression")
+func NewJWTService(secretKey string) *JWTService {
+	return &JWTService{
+		secretKey: []byte(secretKey),
 	}
-	p := &parser{input: expr, pos: 0}
-	node, err := p.parseExpression()
+}
+
+type Claims struct {
+	UserID int64
+	Login  string
+	jwt.RegisteredClaims
+}
+
+func (s *JWTService) GenerateToken(userID int64, login string) (string, error) {
+	claims := &Claims{
+		UserID: userID,
+		Login:  login,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(s.secretKey)
+}
+
+func (s *JWTService) ValidateToken(tokenString string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return s.secretKey, nil
+	})
+
 	if err != nil {
 		return nil, err
 	}
-	if p.pos < len(p.input) {
-		return nil, fmt.Errorf("unexpected token at position %d", p.pos)
-	}
-	return node, nil
-}
 
-type parser struct {
-	input string
-	pos   int
-}
+	if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+		return claims, nil
+	}
 
-func (p *parser) peek() rune {
-	if p.pos < len(p.input) {
-		return rune(p.input[p.pos])
-	}
-	return 0
-}
-
-func (p *parser) get() rune {
-	ch := p.peek()
-	p.pos++
-	return ch
-}
-
-func (p *parser) parseExpression() (*ASTNode, error) {
-	node, err := p.parseTerm()
-	if err != nil {
-		return nil, err
-	}
-	for {
-		ch := p.peek()
-		if ch == '+' || ch == '-' {
-			op := string(p.get())
-			right, err := p.parseTerm()
-			if err != nil {
-				return nil, err
-			}
-			node = &ASTNode{
-				IsLeaf:   false,
-				Operator: op,
-				Left:     node,
-				Right:    right,
-			}
-		} else {
-			break
-		}
-	}
-	return node, nil
-}
-
-func (p *parser) parseTerm() (*ASTNode, error) {
-	node, err := p.parseFactor()
-	if err != nil {
-		return nil, err
-	}
-	for {
-		ch := p.peek()
-		if ch == '*' || ch == '/' {
-			op := string(p.get())
-			right, err := p.parseFactor()
-			if err != nil {
-				return nil, err
-			}
-			node = &ASTNode{
-				IsLeaf:   false,
-				Operator: op,
-				Left:     node,
-				Right:    right,
-			}
-		} else {
-			break
-		}
-	}
-	return node, nil
-}
-
-func (p *parser) parseFactor() (*ASTNode, error) {
-	ch := p.peek()
-	if ch == '(' {
-		p.get()
-		node, err := p.parseExpression()
-		if err != nil {
-			return nil, err
-		}
-		if p.peek() != ')' {
-			return nil, fmt.Errorf("missing closing parenthesis")
-		}
-		p.get()
-		return node, nil
-	}
-	start := p.pos
-	// Обрабатываем знак: унарный плюс разрешаем только если он стоит в начале или сразу после '('
-	if ch == '+' {
-		if p.pos > 0 && p.input[p.pos-1] != '(' {
-			return nil, fmt.Errorf("unexpected unary plus at position %d", p.pos)
-		}
-		p.get()
-	} else if ch == '-' {
-		p.get()
-	}
-	for {
-		ch = p.peek()
-		if unicode.IsDigit(ch) || ch == '.' {
-			p.get()
-		} else {
-			break
-		}
-	}
-	token := p.input[start:p.pos]
-	if token == "" {
-		return nil, fmt.Errorf("expected number at position %d", start)
-	}
-	value, err := strconv.ParseFloat(token, 64)
-	if err != nil {
-		return nil, fmt.Errorf("invalid number %s", token)
-	}
-	return &ASTNode{
-		IsLeaf: true,
-		Value:  value,
-	}, nil
+	return nil, errors.New("invalid token")
 }
