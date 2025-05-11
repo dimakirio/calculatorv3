425fb5a4bf2d67419d941a8af502d5e8acf8241a
dimakirio <kirionenkodima@gmail.com>
2025-05-11 21:35:04 +0500

    Update task.go

--- internal/models/task.go
+++ internal/models/task.go
@@ -1,1 +1,11 @@
+package models
 
+type Task struct {
+	ID            string
+	Arg1          float64
+	Arg2          float64
+	Operation     string
+	OperationTime int
+	Status        string
+	Result        float64 // Добавлено поле Result
+}
